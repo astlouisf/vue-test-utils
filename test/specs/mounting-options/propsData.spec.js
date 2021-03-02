47fd65e6522c61c2b5f4d18cca1d74302e560436
Alexandre St-Louis Fortier <alexandre@timecodelab.com>
2021-03-02 15:32:52 -0500

    test(mounting-options): test propsData containing a 'child' attribute

--- test/specs/mounting-options/propsData.spec.js
+++ test/specs/mounting-options/propsData.spec.js
@@ -31,3 +31,38 @@ describeRunIf(process.env.TEST_ENV !== 'node', 'propsData', () => {
     })
   })
 })
+
+const Component = {
+  template: '<div>{{ child }}</div>',
+  props: ['child']
+}
+
+const childData = {
+  child: 'aBc'
+}
+
+describeRunIf(process.env.TEST_ENV !== 'node', 'propsData {child}', () => {
+  let wrapper
+
+  beforeEach(() => {
+    wrapper = shallowMount(Component, {
+      propsData: childData
+    })
+  })
+
+  afterEach(() => {
+    wrapper = null
+  })
+
+  describe('should not modify propsData between tests', () => {
+    it('should have the correct props after modifying', async () => {
+      expect(wrapper.vm.child).toHaveLength(3)
+      await wrapper.setProps({ child: '' })
+      expect(wrapper.vm.child).toHaveLength(0)
+    })
+
+    it('should have the default props despite being modified in the previous test', () => {
+      expect(wrapper.vm.child).toHaveLength(3)
+    })
+  })
+})
